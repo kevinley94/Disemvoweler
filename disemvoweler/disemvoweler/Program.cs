1c7f50dd1db57f171b312934bc3440c81950ec0f
SeedPaths-User <lessdismissive@email.com>
2014-11-04 18:48:51 -0700

    11/4/2014

--- disemvoweler/disemvoweler/Program.cs
+++ disemvoweler/disemvoweler/Program.cs
@@ -10,35 +10,46 @@ namespace disemvoweler
     {
         static void Main(string[] args)
         {
-            disemvoweler("hello there");
+            disemvoweler("may the force be with you");
             Console.ReadKey();
         }
         static void disemvoweler(string input)
         {
+            //sets containers for vowels
+            //and the letters left over
             string vowel = "";
-            string other = "";
-
+            string remainingLetters = "";
+            //so long as we haven't reached the end of the sentence
+            //the loop will keep going.
             for (int i = 0; i < input.Length; i++)
             {
+                //this will be used later to remove spaces
                 char[] charsToTrim = { ' ' };
+                //this will take a letter from the sentence
+                //lowercase it and make it a string to work with
                 string letter = input[i].ToString().ToLower();
+                // if the statement finds any of the included letters..
                 if ("aeiou".Contains(letter))
                 {
+                    //it will add it to the vowel container
                     vowel = vowel + letter;
                 }
+                    //this is where the spaces get removed
                 else if (letter == " ")
                 {
                     input.Trim(charsToTrim);
                 }
-
-                else { other = other + letter; }
+                    //everything that is left over will be put into
+                    // the remainingLetters container
+                else { remainingLetters = remainingLetters + letter; }
 
 
 
 
             }
+            // Finally print out the results to the console
             Console.WriteLine("Original Sentence: " + input);
-            Console.WriteLine("Disemvoweled Sentence: " + other);
+            Console.WriteLine("Disemvoweled Sentence: " + remainingLetters);
             Console.WriteLine("Vowels Removed: " + vowel);
 
         }
